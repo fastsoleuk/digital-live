adc81137bdb95db8867f7d17b028e21e58fee7ab
hardikaureate <hardik.devani@aureatelabs.com>
2022-05-04 16:33:09 +0530

    FSW:18 - New Changes Update on Live Server

--- packages/mars-theme/src/pages/Productdetails.js
+++ packages/mars-theme/src/pages/Productdetails.js
@@ -40,7 +40,7 @@ const Productdetails = ({ state, libraries, actions }) => {
   const [isMobile] = useMediaQuery("(max-width: 400px)");
   const pageData = state.source.get(state.router.link);
   const seoData = pageData.schemaData && pageData.schemaData;
-  //aureate_console.log("chekcec", pageData);
+  //console.log("chekcec", pageData);
 
   // all custom state
 
@@ -588,8 +588,9 @@ const Productdetails = ({ state, libraries, actions }) => {
                       justifyContent="flex-start"
                     >
                       {status}
-                      {seoData.main.releaseTimeDate != null &&
-                        seoData.main.releaseTimeDate != undefined ? (
+
+                      {/* {seoData.main.releaseTimeDate != null ? seoData.main.releaseTimeDate.date : seoData.main.releaseTimeDate == "01 Jan 1970" ? "TBC" : "fdsf"} */}
+                      {seoData.main.releaseTimeDate != null && seoData.main.releaseTimeDate != undefined ? (
                         <Text
                           color="#7887A5"
                           fontSize="sm"
@@ -597,7 +598,7 @@ const Productdetails = ({ state, libraries, actions }) => {
                           mr="2"
                           ml="2"
                         >
-                          {seoData.main.releaseTimeDate.date}
+                          {seoData.main.releaseTimeDate.date == "Thu, 01 Jan 1970 GMT" ? "TBC" : seoData.main.releaseTimeDate.date} {seoData.main.releaseTimeDate.time ? seoData.main.releaseTimeDate.time : "TBC"}
                         </Text>
                       ) : (
                         <Text
@@ -626,7 +627,7 @@ const Productdetails = ({ state, libraries, actions }) => {
                         seoData.main.releaseTimeDate.styleCode != undefined ? (
                         <Text color="#7887A5" mr="2" fontSize="sm" mb="2">
                           Style Code:
-                          {seoData.main.releaseTimeDate.styleCode}
+                          {` ${seoData.main.releaseTimeDate.styleCode}`}
                         </Text>
                       ) : (
                         <Text
@@ -649,7 +650,6 @@ const Productdetails = ({ state, libraries, actions }) => {
                   color="#3E485D"
                   fontSize="lg"
                   fontWeight="600"
-                  fontWeight="bold"
                   mt={2}
                 >
                   £{seoData.main.releaseTimeDate.price}
@@ -697,6 +697,13 @@ const Productdetails = ({ state, libraries, actions }) => {
                   </Stack>
                 )}
               </Box>
+
+              <Box>
+                <Text color="#3E485D" fontSize="sm" mt={2} mb={2}>
+                  When you buy something from links on this page we may earn a commission. <Link link="/terms-conditions" style={{ fontWeight: 'bold', textDecoration: 'underline' }}>Learn More</Link>
+                </Text>
+              </Box>
+
               {/* for large devices */}
               <Box display={{ base: "none", md: "block" }}>
                 {seoData.main.content && (
@@ -705,11 +712,12 @@ const Productdetails = ({ state, libraries, actions }) => {
                     lineHeight={"26px"}
                     className="product-details detpage"
                   >
+
                     <Html2React html={seoData.main.content} />
                   </Text>
                 )}
               </Box>
-                   
+
               {/* add card componenet here */}
               {seoData.main.releseInfo && (
                 <CardDetails
